26af70f0533f89876f942ba0d98c62d19cdf1bc8
dougssdev <douglassabinosilva@hotmail.com>
2025-03-14 10:08:48 -0300

    Adicionando Docker e Corrigindo erros

--- tasks2do-back/src/main/java/com/br/tasks2do/dto/TarefaResponseDTO.java
+++ tasks2do-back/src/main/java/com/br/tasks2do/dto/TarefaResponseDTO.java
@@ -2,17 +2,15 @@ package com.br.tasks2do.dto;
 
 import com.br.tasks2do.model.tarefas.StatusDaTarefa;
 import com.br.tasks2do.model.tarefas.Tarefas;
-import com.br.tasks2do.model.usuario.Usuario;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
-public record TarefaResponseDTO (Integer id,
-                                 String nome,
-                                 String descricao,
-                                 LocalDate data_de_adicao,
-                                 StatusDaTarefa statusDaTarefa,
-                                 Integer usuarioId){
+public record TarefaResponseDTO(Integer id,
+                                String nome,
+                                String descricao,
+                                LocalDate data_de_adicao,
+                                StatusDaTarefa statusDaTarefa,
+                                Integer usuarioId) {
     public TarefaResponseDTO(Tarefas novaTarefa, int usuarioId) {
         this(novaTarefa.getTarefas_id(), novaTarefa.getNome(),
                 novaTarefa.getDescricao(), novaTarefa.getData_de_adicao(),
